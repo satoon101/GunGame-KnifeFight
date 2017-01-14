f66e0d98a2a6f9af0e3665048e705e2981825ea6
Stephen Toon <satoon101@gmail.com>
2017-01-13 21:19:50 -0500

    Updated rules and added rules translation file.

--- addons/source-python/plugins/gungame/plugins/custom/gg_knife_fight/rules.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_knife_fight/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .info import info
@@ -17,3 +16,4 @@ from .info import info
 # >> RULES
 # =============================================================================
 knife_fight_rules = GunGameRules(info.name)
+knife_fight_rules.register_all_rules()
